e2b52bea5b7876a9930f79512ef02b9003fa454f
Sami <samisamara99@yahoo.com>
2024-02-16 00:36:06 -0600

    - First commit on EmojiPicker

--- EmojiPicker/ContentView.swift
+++ EmojiPicker/ContentView.swift
@@ -2,20 +2,33 @@
 //  ContentView.swift
 //  EmojiPicker
 //
-//  Created by Sereen Samara on 2/16/24.
+//  Created by Sami Samara on 2/15/24.
 //
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😭, 😱, 👺, 😂
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😂
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Picker!")
+            .padding()
         }
-        .padding()
     }
 }
 
