02069b87a62aa2808b5c27bf55acccaf2cb45f76
Sami <samisamara99@yahoo.com>
2024-02-16 00:31:16 -0600

    Initial Commit

---
+++ EmojiPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiPicker
+//
+//  Created by Sereen Samara on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Sereen Samara on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
